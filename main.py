change node splitting from min error percentage to average children error percentage (as in the paper). fix bug in computing error percentage.

main.py
@@ -1,9 +1,11 @@
 import argparse
+import pm4py
 from time import time
 from pm4py.objects.log.importer.xes import importer as xes_importer
-from c4dot5.DecisionTreeClassifier import DecisionTreeClassifier
 from importing import import_attributes, convert_attributes, import_petri_net
-from model_utils import get_sink, activities_to_transitions_map
+from model_utils import get_activities_to_transitions_map
+from algo import MultiplePaths
+from training import train
 
 
 def main():
@@ -21,6 +23,7 @@ def main():
     # Converting attributes types according to the attributes_map file
     log = convert_attributes(attributes_map, log)
 
+    #breakpoint()
     # import model
     petri_net, init_mark, final_mark = import_petri_net(net_name)
 
@@ -28,80 +31,20 @@ def main():
 #    pn_visualizer.view(gviz)
 
     # Dealing with loops and other stuff... needs cleaning
-    activities_to_trans_map = activities_to_transitions_map(petri_net)
+    activities_to_trans_map = get_activities_to_transitions_map(petri_net)
     # TODO declare algo type
 
     tic = time()
     # Scanning the log to get the logs related to decision points
     print('Extracting training logs from Event Log...')
-    decision_points_data, event_attr, stored_dicts = dict(), dict(), dict()
-    variants = variants_filter.get_variants(log)
-    # Decision points of interest are searched considering the variants only
-    for variant in tqdm(variants):
-        transitions_sequence, events_sequence = list(), list()
-        dp_events_sequence = dict()
-        for i, event_name in enumerate(variant.split(',')):
-            trans_from_event = events_to_trans_map[event_name]
-            transitions_sequence.append(trans_from_event)
-            events_sequence.append(event_name)
-            if len(transitions_sequence) > 1:
-                dp_dict, stored_dicts = get_decision_points_and_targets(transitions_sequence, net, stored_dicts)
-                dp_events_sequence['Event_{}'.format(i+1)] = dp_dict
+    algorithm = MultiplePaths(petri_net, activities_to_trans_map)
+    #training_data = algorithm.extract_decision_points_data(log)
+    training_data = algorithm.old_extract_decision_points_data(log)
+    # Data has been gathered. For each decision point, fitting a decision tree on its logs and extracting the rules
+    train(training_data, attributes_map)
+    toc = time()
+    print("\nTotal time: {}".format(toc-tic))
 
-        # Final update of the current trace (from last event to sink)
-        transition = [trans for trans in net.transitions if trans.label == event_name][0]
-        dp_events_sequence['End'] = get_all_dp_from_sink_to_last_event(transition, sink_complete_net, dp_events_sequence)
-
-        for trace in variants[variant]:
-            # Storing the trace attributes (if any)
-            if len(trace.attributes.keys()) > 1 and 'concept:name' in trace.attributes.keys():
-                event_attr.update(trace.attributes)
-
-            # Keeping the same attributes observed in previously (to keep dictionaries at the same length)
-            event_attr = {k: np.nan for k in event_attr.keys()}
-
-            transitions_sequence = list()
-            for i, event in enumerate(trace):
-                trans_from_event = events_to_trans_map[event["concept:name"]]
-                transitions_sequence.append(trans_from_event)
-
-                # Appending the last attribute values to the decision point dictionary
-                if len(transitions_sequence) > 1:
-                    dp_dict = dp_events_sequence['Event_{}'.format(i+1)]
-                    for dp in dp_dict.keys():
-                        # Adding the decision point to the total dictionary if it is not already there
-                        if dp not in decision_points_data.keys():
-                            decision_points_data[dp] = {k: [] for k in ['target']}
-                        for dp_target in dp_dict[dp]:
-                            for a in event_attr.keys():
-                                # Attribute not present and not nan: add it as new key and fill previous entries as nan
-                                if a not in decision_points_data[dp] and event_attr[a] is not np.nan:
-                                    n_entries = len(decision_points_data[dp]['target'])
-                                    decision_points_data[dp][a] = [np.nan] * n_entries
-                                    decision_points_data[dp][a].append(event_attr[a])
-                                # Attribute present: just append it to the existing list
-                                elif a in decision_points_data[dp]:
-                                    decision_points_data[dp][a].append(event_attr[a])
-                            # Appending also the target transition label to the decision point dictionary
-                            decision_points_data[dp]['target'].append(dp_target)
-
-                # Updating the attribute values dictionary with the values from the current event
-                event_attr.update(get_attributes_from_event(event))
-
-            # Appending the last attribute values to the decision point dictionary (from last event to sink)
-            if len(dp_events_sequence['End']) > 0:
-                for dp in dp_events_sequence['End'].keys():
-                    if dp not in decision_points_data.keys():
-                        decision_points_data[dp] = {k: [] for k in ['target']}
-                    for dp_target in dp_events_sequence['End'][dp]:
-                        for a in event_attr.keys():
-                            if a not in decision_points_data[dp] and event_attr[a] is not np.nan:
-                                n_entries = len(decision_points_data[dp]['target'])
-                                decision_points_data[dp][a] = [np.nan] * n_entries
-                                decision_points_data[dp][a].append(event_attr[a])
-                            elif a in decision_points_data[dp]:
-                                decision_points_data[dp][a].append(event_attr[a])
-                        decision_points_data[dp]['target'].append(dp_target)
 
 if __name__ == "__main__":
     main()

processing.py
@@ -1,64 +1,87 @@
-from tqdm import tqdm
-from typing import Union
-from pm4py.algo.filtering.log.variants import variants_filter
-from pm4py.objects.log.obj import EventLog
-from pm4py.objects.petri_net.obj import PetriNet
-from backward_search import get_decision_points_and_targets, get_all_dp_from_sink_to_last_event
+import numpy as np
+from pm4py.objects.log.obj import Trace
 
 
-def extract_decision_points_data(event_log: EventLog, events_to_trans_map: dict, petri_net: PetriNet) -> dict:
-    decision_points_data, event_attr, stored_dicts = dict(), dict(), dict()
-    variants = variants_filter.get_variants(event_log)
-    # Decision points of interest are searched considering the variants only
-    for variant in tqdm(variants):
-        dp_events_sequence = activities_decision_points_map(variant, events_to_trans_map, petri_net, stored_dicts)
-        for trace in variants[variant]:
-            # Storing the trace attributes (if any)
-            if len(trace.attributes.keys()) > 1 and 'concept:name' in trace.attributes.keys():
-                event_attr.update(trace.attributes)
+def get_attributes_from_event(event) -> dict:
+    """ Given an event from an event log, returns a dictionary containing the attributes values of the given event.
 
-            # Keeping the same attributes observed in previously (to keep dictionaries at the same length)
-            event_attr = {k: np.nan for k in event_attr.keys()}
+    Each attribute is set as key of the dictionary, and its value is the actual value of the attribute in the event.
+    Attributes 'time:timestamp' and 'concept:name' are ignored and therefore not added to the dictionary.
+    """
 
-            transitions_sequence = list()
-            for i, event in enumerate(trace):
-                trans_from_event = events_to_trans_map[event["concept:name"]]
-                transitions_sequence.append(trans_from_event)
+    attributes = dict()
+    for attribute in event.keys():
+        if attribute not in ['time:timestamp', 'concept:name']:
+            attributes[attribute] = event[attribute]
+    return attributes
 
-                # Appending the last attribute values to the decision point dictionary
-                if len(transitions_sequence) > 1:
-                    dp_dict = dp_events_sequence['Event_{}'.format(i+1)]
-                    for dp in dp_dict.keys():
-                        # Adding the decision point to the total dictionary if it is not already there
-                        if dp not in decision_points_data.keys():
-                            decision_points_data[dp] = {k: [] for k in ['target']}
-                        for dp_target in dp_dict[dp]:
-                            for a in event_attr.keys():
-                                # Attribute not present and not nan: add it as new key and fill previous entries as nan
-                                if a not in decision_points_data[dp] and event_attr[a] is not np.nan:
-                                    n_entries = len(decision_points_data[dp]['target'])
-                                    decision_points_data[dp][a] = [np.nan] * n_entries
-                                    decision_points_data[dp][a].append(event_attr[a])
-                                # Attribute present: just append it to the existing list
-                                elif a in decision_points_data[dp]:
-                                    decision_points_data[dp][a].append(event_attr[a])
-                            # Appending also the target transition label to the decision point dictionary
-                            decision_points_data[dp]['target'].append(dp_target)
+def insert_attribute(attribute: str, data: dict, decision_point: str) -> dict:
+    n_entries = len(data[decision_point]['target'])
+    data[decision_point][attribute] = [np.nan] * n_entries
+    return data
 
-                # Updating the attribute values dictionary with the values from the current event
-                event_attr.update(get_attributes_from_event(event))
+def insert_decision_point(data: dict, decision_point: str):
+    data[decision_point] = {k: [] for k in ['target']}
+    return data
 
-            # Appending the last attribute values to the decision point dictionary (from last event to sink)
-            if len(dp_events_sequence['End']) > 0:
-                for dp in dp_events_sequence['End'].keys():
-                    if dp not in decision_points_data.keys():
-                        decision_points_data[dp] = {k: [] for k in ['target']}
-                    for dp_target in dp_events_sequence['End'][dp]:
-                        for a in event_attr.keys():
-                            if a not in decision_points_data[dp] and event_attr[a] is not np.nan:
-                                n_entries = len(decision_points_data[dp]['target'])
-                                decision_points_data[dp][a] = [np.nan] * n_entries
-                                decision_points_data[dp][a].append(event_attr[a])
-                            elif a in decision_points_data[dp]:
-                                decision_points_data[dp][a].append(event_attr[a])
-                        decision_points_data[dp]['target'].append(dp_target)
+def insert_nan(decision_points_data: dict, dp: str, event_attr: dict) -> dict:
+    key_dp = set(decision_points_data[dp].keys())
+    key_event = set(event_attr.keys())
+    missing_columns = key_dp.difference(key_event)
+    for column in missing_columns:
+        if column != 'target':
+            decision_points_data[dp][column].append(np.nan)
+    return decision_points_data
+
+def insert_data(event_dps: dict, decision_points_data: dict, event_attr: dict) -> dict:
+    for dp in event_dps.keys():
+        # Adding the decision point to the total dictionary if it is not already there
+        if dp not in decision_points_data.keys():
+            decision_points_data = insert_decision_point(decision_points_data, dp)
+        for dp_target in event_dps[dp]:
+            for attribute in event_attr.keys():
+                # Attribute not present and not nan: add it as new key and fill previous entries as nan
+                if attribute not in decision_points_data[dp] and event_attr[attribute] is not np.nan:
+                    n_entries = len(decision_points_data[dp]['target'])
+                    decision_points_data[dp][attribute] = [np.nan] * n_entries
+                    decision_points_data[dp][attribute].append(event_attr[attribute])
+                # Attribute present: just append it to the existing list
+                elif attribute in decision_points_data[dp]:
+                    decision_points_data[dp][attribute].append(event_attr[attribute])
+            # Appending also the target transition label to the decision point dictionary
+            decision_points_data[dp]['target'].append(dp_target)
+#                if attribute not in decision_points_data[dp]:
+#                    decision_points_data = insert_attribute(attribute, decision_points_data, dp)
+#                # Attribute present: just append it to the existing list
+#                decision_points_data[dp][attribute].append(event_attr[attribute])
+            # insert attributes present in the dataset but not in the event
+            #decision_points_data = insert_nan(decision_points_data, dp, event_attr)
+            # Appending also the target transition label to the decision point dictionary
+            #decision_points_data[dp]['target'].append(dp_target)
+    return decision_points_data
+
+def insert_trace_data(traces: list[Trace], data: dict, event_attr: dict, dp_events_sequence: dict, activities_to_trans_map: dict):
+    for trace in traces:
+        # Storing the trace attributes (if any)
+        if len(trace.attributes.keys()) > 1 and 'concept:name' in trace.attributes.keys():
+            event_attr.update(trace.attributes)
+
+        # Keeping the same attributes observed in previously (to keep dictionaries at the same length)
+        event_attr = {k: np.nan for k in event_attr.keys()}
+
+        transitions_sequence = list()
+        for i, event in enumerate(trace):
+            trans_from_event = activities_to_trans_map[event["concept:name"]]
+            transitions_sequence.append(trans_from_event)
+
+            # Appending the last attribute values to the decision point dictionary
+            if len(transitions_sequence) > 1:
+                dp_dict = dp_events_sequence['Activity_{}'.format(i+1)]
+                data = insert_data(dp_dict, data, event_attr)
+            # Updating the attribute values dictionary with the values from the current event
+            event_attr.update(get_attributes_from_event(event))
+
+        # Appending the last attribute values to the decision point dictionary (from last event to sink)
+        if len(dp_events_sequence['End']) > 0:
+            data = insert_data(dp_events_sequence['End'], data, event_attr)
+    return data